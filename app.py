eb92839e5cdbb15af48852a5dae0de89ed469608
piyush <piyush66oo1000@gmail.com>
2026-09-11 12:44:06 +0530

    start of the project

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def login():
+    return "<p>this is login page!</p>"
+
+@app.route("/faculty")
+def faculty():
+    return "<p>this is  faculty dashboard page.</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
